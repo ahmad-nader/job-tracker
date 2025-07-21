b3320fc2654a2741a15817fe3e2ab5b320346ea3
ahmad-nader <ahmadnaderassal@gmail.com>
2025-07-21 16:31:55 +0000

    refactor: use prisma

--- backend/supabase/migrations/20250524154652_create_interviews_table.sql
+++
@@ -1,7 +0,0 @@
-create table interviews (
-    id uuid primary key default gen_random_uuid(),
-    job_id uuid not null references jobs(id),
-    interview_date date not null,
-    created_at timestamp with time zone default now(),
-    updated_at timestamp with time zone default now()
-);
