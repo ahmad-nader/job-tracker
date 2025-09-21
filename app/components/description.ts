e5a6dd2f4e9302e9f79f2feb282a0b780dde7fc6
ahmnader98 <ahmadnaderassal@gmail.com>
2025-09-21 12:32:04 +0300

    fix: make company name generic

--- app/components/description.ts
+++ app/components/description.ts
@@ -1,4 +1,4 @@
-export const description = `If you are passionate about building breakthrough digital products, and if you thrive in a diverse and inclusive team, b_labs is the perfect opportunity for you. Join us at the forefront of digital retail in Egypt and help shape the future of our industry, one breakthrough at a time.
+export const description = `If you are passionate about building breakthrough digital products, and if you thrive in a diverse and inclusive team, company is the perfect opportunity for you. Join us at the forefront of digital retail in Egypt and help shape the future of our industry, one breakthrough at a time.
 
 Responsibilities
 
@@ -22,9 +22,9 @@ Willingness to continuously learn and improve skills and stay up-to-date with em
 
 Benefits
 
-Office environment: When you come to our b_labs office, you'll find creative workspaces, a well-equipped kitchen, and an open design to foster collaboration between teams.
+Office environment: When you come to our company office, you'll find creative workspaces, a well-equipped kitchen, and an open design to foster collaboration between teams.
 
 Education budget: We believe the learning journey never ends, therefore you will be entitled to an annual education budget to take part in trainings, courses or onsite conferences of your choice.
 
-Flexibility: You know best whether you want to work from home or in the office. However, if you want to join your teammates onsite, we will subsidize the costs for lunch to foster the b_labs office community
+Flexibility: You know best whether you want to work from home or in the office. However, if you want to join your teammates onsite, we will subsidize the costs for lunch to foster the company office community
 `
